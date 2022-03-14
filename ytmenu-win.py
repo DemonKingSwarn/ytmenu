ae50129a97b507567d4fdcdbf84f6a16510d1fe2
DemonKingSwarn <rockingswarn@gmail.com>
2022-03-14 19:58:37 +0530

    switched to vlc

--- ytmenu-win.py
+++ ytmenu-win.py
@@ -3,7 +3,7 @@ import re
 import os
 
 url='https://vid.puffyan.us'
-player="wmplayer.exe"
+player="vlc"
 search=input("Search YouTube: ")
 query=search.replace(" ", "+")
 response=requests.get(f'{url}/search?q={query}')
