b3e1ec1a4a0a0094dfc796ea804ececd395ac563
DemonKingSwarn <rockingswarn@gmail.com>
2022-04-13 19:34:13 +0530

    windows error solved

--- ytmenu-win.py
+++ ytmenu-win.py
@@ -1,17 +1,22 @@
+#!/usr/bin/env python
+
 import requests
 import re
 import os
+import pwd
+
+def get_username():
+    return pwd.getpwuid(os.getuid())[0]
 
 url='https://vid.puffyan.us'
-player="mpv"
-
+player="C://Users//" + get_username() + "//AppData//Roaming//mpv//mpv.exe"
+print(player)
 def search_yt():
     global url
     global player
     search=input("Search YouTube: ")
     query=search.replace(" ", "+")
     response=requests.get(f'{url}/search?q={query}')
-    #string="watch\?v=.{11}"
     x=re.findall(r'(https?://\S+)', response.text)[0]
     url=x.replace("\">", "")
     print(url)
